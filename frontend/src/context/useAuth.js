f5269be642830844cd5e338bfb2a3d7eebdb5ba0
levi <abu.musa.rohan@g.bracu.ac.bd>
2026-09-07 10:48:27 +0600

    chore(ui): update useAuth hook for 2FA context

--- frontend/src/context/useAuth.js
+++ frontend/src/context/useAuth.js
@@ -3,10 +3,8 @@ import AuthContext from './auth-context';
 
 export function useAuth() {
   const context = useContext(AuthContext);
-
-  if (!context) {
-    throw new Error('useAuth must be used inside an AuthProvider.');
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
   }
-
   return context;
 }
